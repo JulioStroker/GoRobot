437a4e874b733db58dd9c06b9d76a6d3e044c2a7
Julio Pinheiro <juliorenan@live.com>
2017-10-17 19:24:01 -0200

    Projeto GoRobot WebCrawler

---
+++ GoRobot/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCrawler;
+namespace GoRobot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Start start = new Start();
+            start.Run();
+        }
+
+        public class Start
+        {
+            public void Run ()
+            {
+                Site site = new Site();
+                site.CarregaPosts();
+            }
+        }
+    }
+}
